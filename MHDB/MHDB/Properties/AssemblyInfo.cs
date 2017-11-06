743141fce5521704dbf8f04a0d30415095140beb
Weston Nudell <Weston.Nudell@gmail.com>
2017-11-06 14:09:11 -0600

    Package and Assembly Info
    Changed Packaging and Assembly Info, such as Display Name, Package Name, Description, etc.

--- MHDB/MHDB/Properties/AssemblyInfo.cs
+++ MHDB/MHDB/Properties/AssemblyInfo.cs
@@ -1,4 +1,5 @@
-﻿using System.Reflection;
+﻿using System.Resources;
+using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
@@ -6,11 +7,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("MHDB")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Military Hardware Database")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("MHDB")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -27,3 +28,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: ComVisible(false)]
+[assembly: NeutralResourcesLanguage("en-US")]
+
